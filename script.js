b748002cdfc81536a0a48de99bf088fc3ba80f2e
Nadya <nadinaeremina@mail.ru>
2024-05-23 11:41:35 +0500

    date

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ while (n--) {
 console.log(data);
 
 const box = document.querySelector(".box");
+box.innerHTML = "<div class=\"card\"></div>";
 
 for (let value of data) {
     box.innerHTML += `<div 
@@ -20,3 +21,23 @@ for (let value of data) {
     style="background-color: ${colors[getNumber(colors.length - 1)]}"
     > ${optimizeNumber(value)}</div>`;
 }
+
+function showTime (day = new Date(2025, 1, 7, 25)) {
+    let now = new Date();
+    let d, h, m, s;
+    let time = Math.floor ((day - now) / 1000);
+    m = Math.floor(time / 60);
+    s = time - m * 60;
+    h = Math.floor (m / 60);
+    m -= h * 60;
+    d = Math.floor(h / 24);
+    h -= d * 24;
+    return `${d} д. ${h} ч. ${m} мин. ${s} сек.`
+}
+
+box.firstElementChild.innerText = `До окончания осталось: ${showTime()}`;
+
+// функция, которая будет делать обратный отсчет через каждые 1000 мс
+setInterval(function(){
+    box.firstElementChild.innerText = `До окончания осталось: ${showTime()}`;
+}, 1000);
